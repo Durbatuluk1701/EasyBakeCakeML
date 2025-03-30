8ba8a8f27b327ae772e8df0e95b4d407ed2e9e82
Will Thomas <30wthomas@gmail.com>
2025-03-30 00:00:59 -0500

    temp3

--- src/common.ml
+++ src/common.ml
@@ -147,7 +147,13 @@ type env = Id.t list * Id.Set.t
 (*s Generic renaming issues for local variable names. *)
 
 let rec rename_id id avoid =
-  if Id.Set.mem id avoid then rename_id (increment_subscript id) avoid else id
+  let id_str = Id.to_string id in
+  let new_id = Id.of_string (if String.is_prefix "_" id_str
+                then "coq_" ^ id_str
+                else id_str) in 
+  (if Id.Set.mem new_id avoid 
+  then rename_id (increment_subscript new_id) avoid 
+  else new_id)
 
 let rec rename_vars avoid = function
   | [] ->
